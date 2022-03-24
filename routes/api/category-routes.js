5c8be393a3de46258a9d431c6ad80e41337fc075
Vaggelis <evangelodamianos@gmail.com>
2022-03-23 23:16:21 -0400

    added category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,8 +5,21 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  Category.findAll()
+  Category.findAll({
+    attributes: [
+      'Shirts',
+      'Shorts',
+      'Music',
+      'Hats',
+      'Shoes'
+    ]
+  })
   // be sure to include its associated Products
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
